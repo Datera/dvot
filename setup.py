b9c5bde59972f6c2934c6c5b372d9337a207cb1a
Matt Smith <mss@datera.io>
2018-08-21 13:42:37 -0500

    Adding setup.py and setup.cfg

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env
+
+from setuptools import setup
+
+setup(
+    name='dvot',
+    version='0.1.0',
+    description='Datera Volume Operations Tool',
+    long_description='Install Instructions: sudo python setup.py install',
+    author='Datera Ecosystem Team',
+    author_email='support@datera.io',
+    packages=['dbmp'],
+    package_dir={'': 'src'},
+    install_requires=[],
+    scripts=[],
+    url='https://github.com/Datera/dvot/',
+)
